43cbeb7a0f3bd133307d6a23238be8533d21f500
joelyustiz <yustizjoel17@gmail.com>
2020-11-16 13:24:47 -0600

    Agregando test de snapshot de footer y header

--- store-app-test/src/__test__/components/Footer.test.js
+++ store-app-test/src/__test__/components/Footer.test.js
@@ -1,14 +1,22 @@
-import React from "react"
-import { mount } from "enzyme"
-import Footer from "../../components/Footer"
+import React from 'react';
+import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
+import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-    const footer = mount(<Footer />)
-    test("render del componente Footer", ()=>{        
-        expect(footer.length).toEqual(1);
-    })
-    test('Render del titulo', ()=>{
-        expect(footer.find('.Footer-title').text()).toEqual("Platzi Store");
-    })
-})
+  const footer = mount(<Footer />);
+  test('render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
 
+describe('footer snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />).toJSON();
+
+    expect(footer).toMatchSnapshot();
+  });
+});
